a3321fff030f80d6456173cce1b5345e55ce2cc1
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-09-25 15:40:37 -0300

    feat - repository funcionario
    criacao do repository funcionario para ter acesso aos comandos sql pre-definidos

---
+++ src/main/java/com/br/apiloja/repository/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package com.br.apiloja.repository;
+
+import com.br.apiloja.entity.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository <Funcionario, Long> {
+}
