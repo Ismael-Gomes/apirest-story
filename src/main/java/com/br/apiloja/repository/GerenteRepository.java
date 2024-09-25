8cb4467af33e1bb033ef36bebd73954f50c23909
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-09-25 15:41:20 -0300

    feat - repository gerente
    criacao do repository gerente para ter acesso aos comandos sql pre-definidos

---
+++ src/main/java/com/br/apiloja/repository/GerenteRepository.java
@@ -0,0 +1,9 @@
+package com.br.apiloja.repository;
+
+import com.br.apiloja.entity.Gerente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GerenteRepository extends JpaRepository <Gerente, Long> {
+}
