c13cdf75e639ad4eb2ddd22f55d65052bc689cde
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-09-25 15:40:06 -0300

    feat - repository cliente
    criacao do repository cliente para ter acesso aos comandos sql pre-definidos

---
+++ src/main/java/com/br/apiloja/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.br.apiloja.repository;
+
+import com.br.apiloja.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, String> {
+}
