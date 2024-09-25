c7c2dcd54e0d46ed53accb35096e2f4b2840c896
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-09-25 15:29:03 -0300

    init - commit inicial
    pacote inicial onde sera desenvolvido o projeto

---
+++ src/main/java/com/br/apiloja/ApilojaApplication.java
@@ -0,0 +1,13 @@
+package com.br.apiloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApilojaApplication.class, args);
+	}
+
+}
