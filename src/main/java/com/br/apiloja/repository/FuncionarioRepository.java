0359cd4d5518c7b3085ec31e6c7cc4713a76c58b
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-09-26 09:28:29 -0300

    chore - substituicao da PK
    foi substituido a chave primaria que antes era tratada como Long e passou a ser um String

--- src/main/java/com/br/apiloja/repository/FuncionarioRepository.java
+++ src/main/java/com/br/apiloja/repository/FuncionarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FuncionarioRepository extends JpaRepository <Funcionario, Long> {
+public interface FuncionarioRepository extends JpaRepository <Funcionario, String> {
 }
