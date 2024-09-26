4d6fe24c72fe2f3e1b13a394e726b34d398b1fe0
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-09-26 09:29:11 -0300

    chore - substituicao do PK
    foi substituido a chave primaria que antes era tratada como Long e passou a ser um String

--- src/main/java/com/br/apiloja/repository/GerenteRepository.java
+++ src/main/java/com/br/apiloja/repository/GerenteRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GerenteRepository extends JpaRepository <Gerente, Long> {
+public interface GerenteRepository extends JpaRepository <Gerente, String> {
 }
