49ecbd190706ebfaa3ef869f54c6548beeff8880
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-09-29 17:31:32 -0300

    feat - adicao de variaveis pessoa
    foi adicionado as variaveis de pessoa que serao utilizadas

--- src/main/java/com/br/apiloja/entity/Pessoa.java
+++ src/main/java/com/br/apiloja/entity/Pessoa.java
@@ -1,10 +1,18 @@
 package com.br.apiloja.entity;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 public class Pessoa {
 
+    private String cpf;
+    private String name;
+    private String email;
+    private String birthday;
+    private String sex;
+    private String situation;
 }
