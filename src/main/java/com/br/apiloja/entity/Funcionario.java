3683cbf44c42b538dc0c2a630dbdb6296334beee
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-09-29 17:28:09 -0300

    cleanup - adicao da notacao funcionario
    foi adicionado as notacoes getter e setter para melhorar a performance do codigo diminuindo linhas que antes precisariam ser escritas

--- src/main/java/com/br/apiloja/entity/Funcionario.java
+++ src/main/java/com/br/apiloja/entity/Funcionario.java
@@ -1,7 +1,12 @@
 package com.br.apiloja.entity;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Funcionario extends Pessoa {
+
 }
