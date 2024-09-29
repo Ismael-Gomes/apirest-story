cee1e2ca90d9187a792bd08768db72e85d54b4ce
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-09-29 17:28:54 -0300

    cleanup - adicao da notacao gerente
    foi adicionado as notacoes getter e setter para melhorar a performance do codigo diminuindo linhas que antes precisariam ser escritas

--- src/main/java/com/br/apiloja/entity/Gerente.java
+++ src/main/java/com/br/apiloja/entity/Gerente.java
@@ -1,8 +1,13 @@
 package com.br.apiloja.entity;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Gerente extends Pessoa {
+
 
 }
