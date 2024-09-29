03628862442cd25883da1f379cdf2d695f0b242e
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-09-29 17:26:54 -0300

    cleanup - adicao de notacao cliente
    foi adicionado as notacoes getter e setter para melhorar a performance do codigo diminuindo linhas que antes precisariam ser escritas

--- src/main/java/com/br/apiloja/entity/Cliente.java
+++ src/main/java/com/br/apiloja/entity/Cliente.java
@@ -1,5 +1,10 @@
 package com.br.apiloja.entity;
+
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 @Entity
+@Getter
+@Setter
 public class Cliente extends Pessoa{
 }
