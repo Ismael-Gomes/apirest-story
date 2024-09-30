7109ec0d1e0719815446cac6cdc1a0aeaed473f0
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-09-30 08:52:32 -0300

    feat - adicao de chaves pk
    foi adicionado chaves que serao unicas na criacao de qualquer pessoa

--- src/main/java/com/br/apiloja/entity/Pessoa.java
+++ src/main/java/com/br/apiloja/entity/Pessoa.java
@@ -1,6 +1,8 @@
 package com.br.apiloja.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +11,10 @@ import lombok.Setter;
 @Setter
 public class Pessoa {
 
+    @Id
     private String cpf;
     private String name;
+    @Column(unique = true)
     private String email;
     private String birthday;
     private String sex;
