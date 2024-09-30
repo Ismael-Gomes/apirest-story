4d768b4770862fc1e4befa4c264cdbe5120a34a1
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-09-30 08:53:04 -0300

    feat - adicao de validacao pessoa
    foi adicionado validacoes do spring validation para garantir que os valores recebidos nao estejam nulos

--- src/main/java/com/br/apiloja/entity/Pessoa.java
+++ src/main/java/com/br/apiloja/entity/Pessoa.java
@@ -3,6 +3,7 @@ package com.br.apiloja.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,10 +14,21 @@ public class Pessoa {
 
     @Id
     private String cpf;
+
+    @NotBlank(message = "Name is mandatory!")
     private String name;
+
     @Column(unique = true)
+    @NotBlank(message = "E-mail is mandatory!")
     private String email;
+
+    @NotBlank(message = "Birthday is mandatory!")
     private String birthday;
+
+    @NotBlank(message = "Sex is mandatory!")
     private String sex;
+
+    @NotBlank(message = "Situation is mandatory!")
     private String situation;
+
 }
