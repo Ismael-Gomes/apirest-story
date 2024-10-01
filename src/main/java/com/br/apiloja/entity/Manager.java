90b0dbd117e5fcc7ceea6f3dc0f80e3ff6665add
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-10-01 09:21:26 -0300

    chore - mudanca de nome
    foi alterado o nome de gerente para "manager"

--- src/main/java/com/br/apiloja/entity/Manager.java
+++ src/main/java/com/br/apiloja/entity/Manager.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Gerente extends Pessoa {
+public class Manager extends Person {
 
     @NotBlank(message = "Position is mandatory!")
     private String position;
