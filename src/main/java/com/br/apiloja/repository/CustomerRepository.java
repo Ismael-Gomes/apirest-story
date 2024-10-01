53441f6d5e2ff80490c38e6923e5ae0bcfc31059
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-10-01 09:23:30 -0300

    chore - mudanca de nome
    foi alterado o nome de cliente para "curtomer"

--- src/main/java/com/br/apiloja/repository/CustomerRepository.java
+++ src/main/java/com/br/apiloja/repository/CustomerRepository.java
@@ -1,9 +1,9 @@
 package com.br.apiloja.repository;
 
-import com.br.apiloja.entity.Cliente;
+import com.br.apiloja.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, String> {
+public interface CustomerRepository extends JpaRepository<Customer, String> {
 }
