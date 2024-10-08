1304d2791330888de3c3f99ae8f620802363110f
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-10-08 15:18:31 -0300

    feat - adicao de buscar todos
    foi adicionado a funcao de buscar todos os clients

--- src/main/java/com/br/apireststory/service/CustomerService.java
+++ src/main/java/com/br/apireststory/service/CustomerService.java
@@ -9,4 +9,8 @@ public class CustomerService {
     public boolean existsCPF(String cpf) {
         return customerRepository.existsById(cpf);
     }
+
+    public List<Customer> listClients() {
+        return customerRepository.findAll();
+    }
 }
